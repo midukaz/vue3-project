c362c37f7e1ef3bf84a1d181c5da5e3418900856
midukaz <paulobarreto758@gmail.com>
2024-11-01 15:50:04 -0300

    first commit

---
+++ src/components/Home.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="flex items-center justify-center min-h-screen">
+        <div class="bg-blue-500 text-white p-6 rounded-md shadow-lg">
+        <h1 class="text-2xl font-bold">Hello Tailwind CSS!</h1>
+        <p>Esta é uma aplicação Vue 3 com Tailwind CSS.</p>
+        </div>
+    </div>
+</template>
+  
